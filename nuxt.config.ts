9925adbfe72f568ba0f0993ad598f568a0814bab
Mathéo Fontaine <matheoftne>
2024-10-03 15:53:49 +0200

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+  components: [
+    '~/src/components',
+    '~/src/pages',
+  ],
+  modules: ['@nuxt/eslint'],
+  eslint: {
+    config: {
+      stylistic: true,
+    },
+  },
+})
